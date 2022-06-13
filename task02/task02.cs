81d965040b46c65a04146413e5ab4870968554c9
nadezhdayu <nadezhdayureva1987@gmail.com>
2022-06-13 17:01:15 +0300

    task02

---
+++ task02/task02.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Console.Write("Введите первое число ");
+string numberString1 = Console.ReadLine();
+Console.Write("Введите второе число ");
+string numberString2 = Console.ReadLine();
+Console.Write("Введите третье число ");
+string numberString3 = Console.ReadLine();
+int numberInt1 = int.Parse(numberString1);
+int numberInt2 = int.Parse(numberString2);
+int numberInt3 = int.Parse(numberString3);
+int max = 0;
+
+
