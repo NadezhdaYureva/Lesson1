45eb57c5754227607c4a172230f5f23451d6b6b8
NadezhdaYureva <106693960+NadezhdaYureva@users.noreply.github.com>
2022-06-13 17:55:41 +0300

    task04

--- task04/task04.cs
+++ task04/task04.cs
@@ -1,4 +1,5 @@
 ﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
 Console.Write("Введите число ");
 string number = Console.ReadLine();
 int numberInt = int.Parse(number);
